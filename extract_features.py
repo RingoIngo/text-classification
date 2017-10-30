b8421409c77950042839ba781e84fb99f3a887f4
Ingo Guehring <i.guehring@gmx.de>
2017-10-30 23:34:43 +0100

    add: normalize and filter tokens

--- extract_features.py
+++ extract_features.py
@@ -1,5 +1,15 @@
+#-*- coding: UTF-8 -*-
 # import numpy as np
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import csv
+import nltk
+
+SPECIAL_CHARACTERS = [(u'Ä', 'Ae'), ('ä', 'ae'),
+                      (u'Ö', 'Oe'), (u'ö', 'oe'),
+                      (u'Ü', 'Ue'), (u'ü', 'ue'),
+                      (u'ß', 'ss')]
 
 
 def extract_features(qfile='question_train.csv',
@@ -13,19 +23,49 @@ def extract_features(qfile='question_train.csv',
 #    print(categories)
 #    print(parentdic)
 #    print(questions[0])
+    stopWords = set(stopwords.words('german'))
+    normQuestions = []
+    for (question, cat) in questions:
+        print(question)
+        # print(nltk.tokenize.wordpunct_tokenize(q[0]))
+        # adviced tokenizer
+        tokens = word_tokenize(question, language='german')
+        # normalize tokens
+        # TODO: normalize, i.e. small case, spell check, map numebers and dates
+        normtokens = [_normalize(z) for z in tokens if z.lower() not in stopWords]
+        cleanNormTokens = list(filter(
+            lambda w: _removeStopWordsAndPunc(w, stopWords), normtokens))
+        normQuestions.append((cleanNormTokens, cat))
+        print(cleanNormTokens)
     return None
 
 
+def _removeStopWordsAndPunc(word, stopWords):
+    return ((word not in stopWords)
+            and not (all(c in string.punctuation for c in word)))
+
+
+def _normalize(word):
+    word = word.lower()
+    for (Umlaut, replacement) in SPECIAL_CHARACTERS:
+        word = word.replace(Umlaut, replacement)
+    return word
+
+
 def _readcat(catfile, subcats):
+    categories = {}
+    parentdic = {}
     with open(catfile, 'rU') as f:
         reader = csv.reader(f, quotechar='"', delimiter=',')
-        categories = {}
-        parentdic = {}
+        f_header = next(reader)
+        category_id_idx = f_header.index("category_id")
+        parent_id_idx = f_header.index("parent_id")
+        category_name_idx = f_header.index("category_name")
         for rowno, row in enumerate(reader):
             try:
-                category_id = int(row[0])
-                parent_id = int(row[1])
-                category_name = row[2]
+                category_id = int(row[category_id_idx])
+                parent_id = int(row[parent_id_idx])
+                category_name = row[category_name_idx]
                 parentdic[category_id] = parent_id
                 if ((subcats and parent_id != 0)
                         or (not subcats and parent_id == 0)):
