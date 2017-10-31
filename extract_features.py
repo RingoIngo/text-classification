6cf3d6c46277dfba000994764cb4651442272783
Ingo Guehring <i.guehring@gmx.de>
2017-10-31 21:17:56 +0100

    add: spellcheck, stem, normalization

--- extract_features.py
+++ extract_features.py
@@ -1,10 +1,12 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 # import numpy as np
 import string
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.tokenize import WordPunctTokenizer
 from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 import csv
-import nltk
+from correctors import SpellingCorrector
 
 SPECIAL_CHARACTERS = [(u'Ä', 'Ae'), ('ä', 'ae'),
                       (u'Ö', 'Oe'), (u'ö', 'oe'),
@@ -15,7 +17,10 @@ SPECIAL_CHARACTERS = [(u'Ä', 'Ae'), ('ä', 'ae'),
 def extract_features(qfile='question_train.csv',
                      qcatfile='question_category_train.csv',
                      catfile='category.csv',
+                     spellcorrection=True,
+                     stemming=True,
                      subcats=True,
+                     tokenization='word_tokenize',
                      outfile='features.npz'):
 
     categories, parentdic = _readcat(catfile, subcats)
@@ -23,32 +28,69 @@ def extract_features(qfile='question_train.csv',
 #    print(categories)
 #    print(parentdic)
 #    print(questions[0])
-    stopWords = set(stopwords.words('german'))
+    if tokenization == 'word_punct_tokenize':
+        tokenizer = WordPunctTokenizer()
+    else:
+        # adviced tokenizer TODO: provide reference
+        tokenizer = TreebankWordTokenizer()
+    spellcorrector = SpellingCorrector() if spellcorrection else None
+    stemmer = SnowballStemmer('german') if stemming else None
+    # clean questions
+    token_filter = _make_stopwords_and_punct_filter(
+        set(stopwords.words('german')))
     normQuestions = []
-    for (question, cat) in questions:
-        print(question)
-        # print(nltk.tokenize.wordpunct_tokenize(q[0]))
-        # adviced tokenizer
-        tokens = word_tokenize(question, language='german')
-        # normalize tokens
-        # TODO: normalize, i.e. small case, spell check, map numebers and dates
-        normtokens = [_normalize(z) for z in tokens if z.lower() not in stopWords]
-        cleanNormTokens = list(filter(
-            lambda w: _removeStopWordsAndPunc(w, stopWords), normtokens))
-        normQuestions.append((cleanNormTokens, cat))
-        print(cleanNormTokens)
+    for i, (question, cat) in enumerate(questions):
+        clean_question = tokenize_and_clean(question,
+                                            spellcorrector, stemmer,
+                                            token_filter,
+                                            tokenizer)
+        # add tokens to dictionary set??
+        normQuestions.append((clean_question, cat))
+        if i < 100:
+            print(question)
+            print(clean_question)
+    # create dictionary from questions
+    # TODO
+
+    # extract features from cleaned questions
+    # TODO
     return None
 
 
-def _removeStopWordsAndPunc(word, stopWords):
-    return ((word not in stopWords)
-            and not (all(c in string.punctuation for c in word)))
+def tokenize_and_clean(question, spellcorrector,
+                       stemmer, token_filter, tokenizer):
+    # TODO is language='german' a valid argument?
+    tokens = tokenizer.tokenize(question)
+    if spellcorrector is not None:
+        # spellcorrect only tokens that are not filtered
+        # since stopwords are in small case we filter .lower()
+        tokens = [spellcorrector.correct(w) for w in tokens if
+                  token_filter(w.lower())]
+    # normalize tokens
+    # tokens = [lambda w: _normalize(w, spellcorrected) for w in tokens if
+    #           token_filter(w.lower())]
+    tokens = [_normalize(w, spellcorrector is not None) for w in tokens if
+              token_filter(w.lower())]
+    # stemm tokens
+    if stemmer is not None:
+        tokens = [stemmer.stem(w) for w in tokens]
+    return tokens
 
 
-def _normalize(word):
+def _make_stopwords_and_punct_filter(stopWords):
+    def remove_stop_words_and_punctuation(word):
+        return ((word not in stopWords)
+                and not all(c in string.punctuation for c in word))
+    return remove_stop_words_and_punctuation
+
+
+def _normalize(word, spellcorrected):
     word = word.lower()
-    for (Umlaut, replacement) in SPECIAL_CHARACTERS:
-        word = word.replace(Umlaut, replacement)
+    # if tokens are not spellcorrected, unify special character
+    if not spellcorrected:
+        for (Umlaut, replacement) in SPECIAL_CHARACTERS:
+            word = word.replace(Umlaut, replacement)
+    # TODO: deal with numbers and dates
     return word
 
 
