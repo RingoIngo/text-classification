bbf3047186aba180ec37e67ef64f54579a4e2ac5
Ingo Guehring <i.guehring@gmx.de>
2017-11-08 17:11:12 +0100

    refactor model for FeatureUnion

--- smsguru_model.py
+++ smsguru_model.py
@@ -8,6 +8,8 @@ from time import gmtime, strftime
 import numpy as np
 
 from sklearn.decomposition import TruncatedSVD
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
@@ -18,10 +20,12 @@ from sklearn.naive_bayes import MultinomialNB
 import text_tokenizer_and_cleaner as ttc
 import question_loader as ql
 
-# taken from scikit learn examples:
-# "Feature Union with Heterogeneous Data Sources"
+
 class ItemSelector(BaseEstimator, TransformerMixin):
     """For data grouped by feature, select subset of data at a provided key.
+
+    Taken from scikit learn examples:
+    `Feature Union with Heterogeneous Data Sources`
 
     The data is expected to be stored in a 2D data structure, where the first
     index is over features and the second is over samples.  i.e.
@@ -59,31 +63,44 @@ class ItemSelector(BaseEstimator, TransformerMixin):
         return data_dict[self.key]
 
 
-class SubjectBodyExtractor(BaseEstimator, TransformerMixin):
-    """Extract the subject & body from a usenet post in a single pass.
+class ListVectorizer(BaseEstimator, TransformerMixin):
+    """Vectorize a list of features.
 
-    Takes a sequence of strings and produces a dict of sequences.  Keys are
-    `subject` and `body`.
+    Convert a list of numerical features into the from scikit expected format.
+    This list is expected to have a length equal to the number of samples.
+    Returns a 2-D array, since format (n,) throws an exception in scikit.
+    Type of the return array must be float.
+    See also:
+
+    https://stackoverflow.com/questions/22273242/
+    scipy-hstack-results-in-typeerror-no-supported-conversion
+    -for-types-dtype
     """
     def fit(self, x, y=None):
         return self
 
-    def transform(self, posts):
-        features = np.recarray(shape=(len(posts),),
-                               dtype=[('subject', object), ('body', object)])
-        for i, text in enumerate(posts):
-            headers, _, bod = text.partition('\n\n')
-            bod = strip_newsgroup_footer(bod)
-            bod = strip_newsgroup_quoting(bod)
-            features['body'][i] = bod
+    def transform(self, feature_list):
+        # this is the from scikit expected format
+        feature_vector = np.asarray(feature_list).reshape(len(feature_list), 1)
+        return feature_vector.astype(float)
 
-            prefix = 'Subject:'
-            sub = ''
-            for line in headers.split('\n'):
-                if line.startswith(prefix):
-                    sub = line[len(prefix):]
-                    break
-            features['subject'][i] = sub
+
+class QuestionTimeExtractor(BaseEstimator, TransformerMixin):
+    """Extract the question & date.
+
+    Takes a list of tuples (question, creation_date) and produces a dict of
+    sequences.  Keys are `question` and `time`.
+    """
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, question_dates):
+        features = np.recarray(shape=(len(question_dates),),
+                               dtype=[('question', object), ('time', object)])
+        for i, quest_date_tuple in enumerate(question_dates):
+            features['question'][i] = quest_date_tuple['question']
+            # only the time is extracted
+            features['time'][i] = int(quest_date_tuple['date'].strftime('%H'))
 
         return features
 
@@ -92,40 +109,67 @@ def _identity(words):
     return words
 
 
-def create_pipeline(estimator=None):
+def create_pipeline(classifier=None):
     """Chain processing steps
 
     Build a `Pipeline` in which all processing steps are chained.
-    1. tokenize and clean text
-    2. vectorize tokens
-    3. tfidf weighting
-    4. TruncatedSVD reduction
-    5. estimator (optinal)
+    1. prepare heterogeneous data
+        1.1 select textual data (sms questions)
+            1.1.1. tokenize and clean text
+            1.1.2. vectorize tokens
+            1.1.3. tfidf weighting
+            1.1.4. TruncatedSVD reduction
+        1.2 select creation dates
+            1.2.1. vectorize
+    2. classifier (optional)
 
     Parameters
     ----------
-    estimator : an estimator, optinal, which is applied as last step
+    classifier : an classifier, optinal, which is applied as last step
         in the chain. Default is None.
     """
 
     steps = [
-        ('tokens', ttc.TextTokenizerAndCleaner()),
-        ('vectorize', CountVectorizer(tokenizer=_identity,
-                                      preprocessor=None,
-                                      lowercase=False)),
-        ('tfidf', TfidfTransformer()),
-        ('reduce_dim', SelectKBest(chi2, k=500))
+        # Extract the question & its creation time
+        ('question_time', QuestionTimeExtractor()),
+
+        # Use FeatureUnion to combine the features from question and time
+        ('union', FeatureUnion(
+            transformer_list=[
+
+                # Pipeline for pulling features from the question itself
+                ('question_bow', Pipeline([
+                    ('selector', ItemSelector(key='question')),
+                    ('tokens', ttc.TextTokenizerAndCleaner()),
+                    ('vectorize', CountVectorizer(tokenizer=_identity,
+                                                  preprocessor=None,
+                                                  lowercase=False)),
+                    ('tfidf', TfidfTransformer()),
+                    ('reduce_dim', SelectKBest(chi2, k=500)),
+                ])),
+
+                # Pipeline for creation time
+                ('creation_time', Pipeline([
+                    ('selector', ItemSelector(key='time')),
+                    ('vectorize', ListVectorizer()),
+                ])),
+            ],
+
+            # weight components in FeatureUnion
+            transformer_weights=None,
+        )),
     ]
 
-    if estimator is not None:
-        # Add the estimator
-        steps.append(('classifier', estimator))
+    if classifier is not None:
+        # Add a classifier to the combined features
+        steps.append(('classifier', classifier))
     return Pipeline(steps)
 
 
 def evaluate_model(qfile='question_train.csv',
                    catfile='category.csv',
                    subcats=True,
+                   metadata=True,
                    verbose=10):
     CV = 5
     # the dimensions used in the dim reduction step
@@ -137,49 +181,49 @@ def evaluate_model(qfile='question_train.csv',
     MIN_DF = [1, 2, 3]
 
     # model = create_pipeline(KNeighborsClassifier())
-    model = create_pipeline(MultinomialNB())
+    model = create_pipeline(classifier=MultinomialNB())
     # param_grid = [
     #     # multivariate feature selection
-    #     dict(tokens__mapdates=[True, False],
-    #          tokens__mapnumbers=[True, False],
-    #          tokens__spellcorrect=[True, False],
-    #          tokens__stem=[True, False],
-    #          tokens__tokenizer=['word_tokenizer',
+    #     dict(union__question_bow__tokens__mapdates=[True, False],
+    #          union__question_bow__tokens__mapnumbers=[True, False],
+    #          union__question_bow__tokens__spellcorrect=[True, False],
+    #          union__question_bow__tokens__stem=[True, False],
+    #          union__question_bow__tokens__tokenizer=['word_tokenizer',
     #                             'word_punct_tokenizer'],
-    #          vectorize__binary=[True, False],
-    #          vectorize__min_df=MIN_DF,
-    #          tfidf=[None, TfidfTransformer()],
-    #          reduce_dim=[TruncatedSVD()],
-    #          reduce_dim__n_components=N_DIM_OPTIONS
+    #          union__question_bow__vectorize__binary=[True, False],
+    #          union__question_bow__vectorize__min_df=MIN_DF,
+    #          union__question_bow__tfidf=[None, TfidfTransformer()],
+    #          union__question_bow__reduce_dim=[TruncatedSVD()],
+    #          union__question_bow__reduce_dim__n_components=N_DIM_OPTIONS
     #          ),
     #     # univariate feature selection
-    #     dict(tokens__mapdates=[True, False],
-    #          tokens__mapnumbers=[True, False],
-    #          tokens__spellcorrect=[False],
-    #          tokens__stem=[True, False],
-    #          tokens__tokenizer=['word_tokenizer',
+    #     dict(union__question_bow__tokens__mapdates=[True, False],
+    #          union__question_bow__tokens__mapnumbers=[True, False],
+    #          union__question_bow__tokens__spellcorrect=[False],
+    #          union__question_bow__tokens__stem=[True, False],
+    #          union__question_bow__tokens__tokenizer=['word_tokenizer',
     #                             'word_punct_tokenizer'],
-    #          vectorize__binary=[True, False],
-    #          vectorize__min_df=MIN_DF,
-    #          tfidf=[None, TfidfTransformer()],
-    #          reduce_dim=[SelectKBest(chi2)],
-    #          reduce_dim__k=N_DIM_OPTIONS,
+    #          union__question_bow__vectorize__binary=[True, False],
+    #          union__question_bow__vectorize__min_df=MIN_DF,
+    #          union__question_bow__tfidf=[None, TfidfTransformer()],
+    #          union__question_bow__reduce_dim=[SelectKBest(chi2)],
+    #          union__question_bow__reduce_dim__k=N_DIM_OPTIONS,
     #          )
     # ]
     # TODO: n_jobs could be an interesting option when on cluster
 
     # TEST
     param_grid = [
-        dict(tokens__mapdates=[False],
-             tokens__mapnumbers=[False],
-             tokens__spellcorrect=[False],
-             tokens__stem=[False],
-             tokens__tokenizer=['word_punct_tokenizer'],
-             vectorize__binary=[False],
-             vectorize__min_df=[1, 2],
-             tfidf=[None],
-             reduce_dim=[SelectKBest(chi2)],
-             reduce_dim__k=[500],
+        dict(union__question_bow__tokens__mapdates=[False],
+             union__question_bow__tokens__mapnumbers=[False],
+             union__question_bow__tokens__spellcorrect=[False],
+             union__question_bow__tokens__stem=[False],
+             union__question_bow__tokens__tokenizer=['word_punct_tokenizer'],
+             union__question_bow__vectorize__binary=[False],
+             union__question_bow__vectorize__min_df=[1, 2],
+             union__question_bow__tfidf=[None],
+             union__question_bow__reduce_dim=[SelectKBest(chi2)],
+             union__question_bow__reduce_dim__k=[500],
              )
     ]
     grid_search = GridSearchCV(model, cv=CV,
@@ -189,7 +233,9 @@ def evaluate_model(qfile='question_train.csv',
                                refit=False,
                                verbose=verbose)
     loader = ql.QuestionLoader(qfile=qfile, catfile=catfile,
-                               subcats=subcats, verbose=verbose)
+                               subcats=subcats,
+                               metadata=metadata,
+                               verbose=verbose)
     grid_search.fit(loader.questions, loader.categoryids)
     return grid_search
 
