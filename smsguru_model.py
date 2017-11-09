a96f94b1aa81d48b239a71d6c921046a7cd17073
Ingo Guehring <i.guehring@gmx.de>
2017-11-10 00:51:52 +0100

    squash

--- smsguru_model.py
+++ smsguru_model.py
@@ -502,7 +502,7 @@ if __name__ == "__main__":
 
     sms_guru_model = (SMSGuruModel(classifier=classifier, metadata=metadata).
                       set_question_loader(subcats=subcats).
-                      gridsearch(test_param_grid))
+                      gridsearch(grid))
     current_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
     np.save('./results/gridsearch/' + current_time + 'grids_cv.npy',
             sms_guru_model.grid_search_.cv_results_)
