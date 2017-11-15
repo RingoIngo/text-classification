4956fc6940acd07cc06b8d59c18d2db8fbfbd0cd
mlp_01 <mlp_01@vnc.ml.tu-berlin.de>
2017-11-15 15:15:37 +0100

    add gridsearches

--- run_on_cluster.py
+++ run_on_cluster.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import smsguru_model as sms
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -14,18 +15,17 @@ if __name__ == "__main__":
         return z
 
     classifier = MultinomialNB()
-    subcats = False
+    subcats = True
     metadata = False
+    CV = 3
     # the dimensions used in the dim reduction step
-    N_DIM_OPTIONS = [10, 30, 60, 100, 200, 500, 1000, 1500, 2500, 5000]
+    N_DIM_OPTIONS = [50, 100, 200, 500, 1000, 1500, 2500]
 
     # this choice is based on [Seb99]
     MIN_DF = [1, 2, 3]
 
     base_grid = (
-        dict(union__question_bow__tokens__mapdates=[True, False],
-             union__question_bow__tokens__mapnumbers=[True, False],
-             union__question_bow__tokens__spellcorrect=[True, False],
+        dict(union__question_bow__tokens__spellcorrect=[True, False],
              union__question_bow__tokens__stem=[True, False],
              # union__question_bow__tokens__tokenizer=['word_tokenizer',
              #                                         'word_punct_tokenizer'],
@@ -34,6 +34,17 @@ if __name__ == "__main__":
              union__question_bow__vectorize__min_df=MIN_DF,
              union__question_bow__tfidf=[None, TfidfTransformer()],
              )
+    )
+
+    mapnumerics = (
+        dict(union__question_bow__tokens__mapdates=[True],
+             union__question_bow__tokens__mapnumbers=[True])
+    )
+
+
+    no_mapnumerics = (
+        dict(union__question_bow__tokens__mapdates=[False],
+             union__question_bow__tokens__mapnumbers=[False])
     )
 
     univariate = (
@@ -46,31 +57,31 @@ if __name__ == "__main__":
              union__question_bow__reduce_dim__n_components=N_DIM_OPTIONS)
     )
 
-    grid = [merge_two_dicts(base_grid, univariate),
-            merge_two_dicts(base_grid, multivariate)]
+    grid = [reduce(merge_two_dicts, [base_grid, mapnumerics, univariate]),
+            reduce(merge_two_dicts, [base_grid, no_mapnumerics, multivariate])]
 
     # TEST
     test_param_grid = [
-        dict(union__question_bow__tokens__mapdates=[False],
-             union__question_bow__tokens__mapnumbers=[False],
+        dict(union__question_bow__tokens__mapdates=[True],
+             union__question_bow__tokens__mapnumbers=[True],
              union__question_bow__tokens__spellcorrect=[False],
              union__question_bow__tokens__stem=[False],
              union__question_bow__tokens__tokenizer=['word_punct_tokenizer'],
              union__question_bow__vectorize__binary=[False],
-             union__question_bow__vectorize__min_df=[1, 2],
+             union__question_bow__vectorize__min_df=[3],
              union__question_bow__tfidf=[None],
              union__question_bow__reduce_dim=[SelectKBest(chi2)],
-             union__question_bow__reduce_dim__k=[2000],
+             union__question_bow__reduce_dim__k=[8000],
              ),
     ]
 
     sms_guru_model = (sms.SMSGuruModel(classifier=classifier, metadata=metadata).
                       set_question_loader(subcats=subcats).
-                      gridsearch(param_grid=test_param_grid,n_jobs=-1))
+                      gridsearch(param_grid=grid, n_jobs=-1, CV=CV))
     current_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
     np.save('./results/gridsearch/' + current_time + 'grids_cv.npy',
             sms_guru_model.grid_search_.cv_results_)
     with open("./results/gridsearch/gridsearches.txt", "a") as report:
         report.write("""performed at: {}, non_grid_params:  subcats: {},
-                      metadata: {}, classifier: MultinomialNB\n"""
-                     .format(current_time, subcats, metadata))
+                      metadata: {}, CV: {}, classifier: MultinomialNB\n"""
+                     .format(current_time, subcats, metadata, CV))
