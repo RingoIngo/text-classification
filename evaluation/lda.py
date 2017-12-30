eef04c30a41ba12588dcd356f230555556f199f8
Ingo Guehring <i.guehring@gmx.de>
2017-12-30 20:23:35 +0100

    refactor lda

--- evaluation/lda.py
+++ evaluation/lda.py
@@ -19,7 +19,16 @@ def evaluate(gridsearch=True, gen_error=True):
     # since there are no hyper parameters to be optimized we only need
     # the generalization error estimate
     MODEL.set_question_loader(subcats=shared.SUBCATS)
-    nested_scores = MODEL.nested_cv(param_grid=PARAM_GRID, CV=shared.CV)
-    shared.save_and_report(results=nested_scores,
-                           folder='lda',
-                           name='gen_error.npy')
+    if gridsearch:
+        MODEL.gridsearch(param_grid=PARAM_GRID,
+                         n_jobs=shared.N_JOBS,
+                         CV=shared.CV)
+        shared.save_and_report(
+            results=MODEL.grid_search_.cv_results_,
+            folder='lda')
+
+    if gen_error:
+        nested_scores = MODEL.nested_cv(param_grid=PARAM_GRID, CV=shared.CV)
+        shared.save_and_report(results=nested_scores,
+                               folder='lda',
+                               name='gen_error.npy')
