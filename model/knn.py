5ca7586104261b926b27f8755616916c32042cf9
Ingo Guehring <i.guehring@gmx.de>
2017-12-31 15:38:03 +0100

    add probability support for knn_b

--- model/knn.py
+++ model/knn.py
@@ -91,3 +91,51 @@ class KNeighborsClassifierB(KNeighborsClassifier):
             label_counts[i, :] = weighted_counts / total
         y_pred = self.classes_[np.argmax(label_counts, axis=0)]
         return y_pred
+
+    def predict_proba(self, X):
+        """Return probability estimates for the test data X.
+        Parameters
+         ----------
+        X : array-like, shape (n_query, n_features), \
+            or (n_query, n_indexed) if metric == 'precomputed'
+            Test samples.
+        Returns
+        -------
+        y : array of shape [n_samples] or [n_samples, n_outputs]
+            Class labels for each data sample.
+        """
+        X = check_array(X, accept_sparse='csr')
+
+        neigh_dist, neigh_ind = self.kneighbors(X)
+
+        # bcz of numerical reasons we don't use the mode approach here
+
+        neigh_sim = cosine_dist_to_sim(neigh_dist)
+        # self._y does not contain labels!
+        # it contains the indices of the labels in self.classes_
+        # see SupervisedIntegerMixin fit() method
+        neigh_labels = self._y[neigh_ind]
+        # dict where lables are keys and
+        # items arrays ([n_samples]), the label probs
+        n_samples = X.shape[0]
+        classes = self.classes_
+        n_classes = len(classes)
+        label_counts = np.empty((n_classes, n_samples))
+        for i, label in enumerate(classes):
+            label_top_n = self.top_n[label]
+            # label_top_n_sim.shape = [n_sample,label_top_n]
+            label_top_n_sim = neigh_sim[
+                np.arange(n_samples)[:, np.newaxis], np.arange(label_top_n)]
+            label_top_n_labels_idx = neigh_labels[
+                np.arange(n_samples)[:, np.newaxis], np.arange(label_top_n)]
+            # total.shape = [n_samples,]
+            total = np.sum(label_top_n_sim, axis=1)
+            label_idx = np.where(classes == label)[0][0]
+            weighted_counts = np.sum(
+                label_top_n_sim * (label_top_n_labels_idx == label_idx),
+                axis=1)
+            label_counts[i, :] = weighted_counts / total
+        normalizer = label_counts.sum(axis=0)
+        normalizer[normalizer == 0] = 1.0
+        label_counts = label_counts / normalizer
+        return label_counts.T
