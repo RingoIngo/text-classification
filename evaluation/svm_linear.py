da9e4cdbeae1d21f6ac7eab149369d497c81e0f9
Ingo Guehring <i.guehring@gmx.de>
2018-01-08 17:32:41 +0100

    add linear svm

--- evaluation/svm_linear.py
+++ evaluation/svm_linear.py
@@ -1,35 +1,25 @@
 """
-The :mod: `svm` implements the model and the constants needed
-for the evalutation of SVM as classifier"""
+The :mod: `svm_linear` implements the model and the constants needed
+for the evalutation of SVM as classifier with a linear kernel"""
 # Author: Ingo Guehring
 
-# import numpy as np
-from sklearn.svm import SVC
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.random_projection import SparseRandomProjection
+import numpy as np
+from sklearn.svm import LinearSVC
 
 import evaluation.shared as shared
 import model
 
 
-reduction = SparseRandomProjection(n_components=500)
-CLASSIFIER = OneVsRestClassifier(SVC(probability=False))
-
-# grid
-# kernels = ['linear', 'rbf']
-# finer grid than in lda_svm since less combinations
-# GAMMA_RANGE = np.logspace(-3, 3, 10)
-# C_RANGE = np.logspace(-3, 3, 10)
+CLASSIFIER = LinearSVC()
 
 # new wider range
-C_RANGE = shared.C_RANGE
-GAMMA_RANGE = shared.GAMMA_RANGE
+# C_RANGE = shared.C_RANGE
+C_RANGE = np.logspace(-5, 5, 11)
 
-PARAM_GRID = [dict(classifier__estimator__gamma=GAMMA_RANGE,
-                   classifier__estimator__C=C_RANGE)]
+PARAM_GRID = [dict(classifier__estimator__C=C_RANGE)]
 
 # model for use in train_apply_classifier
-MODEL = model.SMSGuruModel(classifier=CLASSIFIER, reduction=reduction)
+MODEL = model.SMSGuruModel(classifier=CLASSIFIER)
 
 
 def evaluate(gridsearch=True, gen_error=True, memory=True):
@@ -52,18 +42,17 @@ def evaluate(gridsearch=True, gen_error=True, memory=True):
     ---------
     NOTHING but SAVES the results of the performed computations
     """
-    MODEL = model.SMSGuruModel(classifier=CLASSIFIER, reduction=reduction,
-                               memory=memory)
+    MODEL = model.SMSGuruModel(classifier=CLASSIFIER, memory=memory)
     MODEL.set_question_loader(subcats=shared.SUBCATS)
     if gridsearch:
         MODEL.gridsearch(param_grid=PARAM_GRID, n_jobs=shared.N_JOBS,
                          CV=shared.CV)
         shared.save_and_report(
             results=MODEL.grid_search_.cv_results_,
-            folder='svm')
+            folder='svm_linear')
 
     if gen_error:
         nested_scores = MODEL.nested_cv(param_grid=PARAM_GRID, CV=shared.CV)
         shared.save_and_report(results=nested_scores,
-                               folder='svm',
+                               folder='svm_linear',
                                name='gen_error.npy')
