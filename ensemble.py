b5c2bbcdcf909dbe2ab8121b4c3cfd79ff4a683d
Ingo Guehring <i.guehring@gmx.de>
2018-02-16 23:59:18 +0100

    exchange svm with logreg in ensemble

--- ensemble.py
+++ ensemble.py
@@ -7,7 +7,9 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, cross_val_score
+import numpy as np
 
 from model import SMSGuruModel
 import model.question_loader as ql
@@ -18,20 +20,39 @@ mnb = SMSGuruModel(classifier=MultinomialNB(), reduction=None,
                    metadata=False, memory=True).model
 svm = SMSGuruModel(classifier=LinearSVC(C=0.1), reduction=None).model
 lda = SMSGuruModel(classifier=LDA(), reduction=None, memory=True).model
+logreg = SMSGuruModel(classifier=LogisticRegression(), reduction=None).model
 
 ensemble = VotingClassifier(
-    estimators=[('mnb', mnb), ('svm', svm), ('lda', lda)], voting='soft')
+    estimators=[('mnb', mnb), ('logreg', logreg), ('lda', lda)], voting='soft')
 
 qfile = './data/question_train.csv'
 catfile = './data/category.csv'
 subcats = False
+cv = 5
+verbose = 100
 
+# without gridsearch
 question_loader = ql.QuestionLoader(
     qfile=qfile, catfile=catfile, subcats=subcats, metadata=True, verbose=True)
 
 scores = cross_val_score(
-    ensemble, question_loader.questions, question_loader.categoryids, cv=5,
-    scoring='f1_macro', n_jobs=-1, verbose=100)
+    ensemble, question_loader.questions, question_loader.categoryids, cv=cv,
+    scoring='f1_macro', n_jobs=-1, verbose=verbose)
 
 shared.save_and_report(
     results=scores, folder='ensemble', name='gen_error.npy')
+
+# with gridsearch
+
+# svm param
+C_RANGE = np.logspace(-5, 5, 11)
+
+# grid
+PARAM_GRID = {'logreg__classifier__C': C_RANGE}
+
+grid = GridSearchCV(
+    estimator=ensemble, cv=cv, param_grid=PARAM_GRID, scoring='f1_macro',
+    refit=False, error_score=-1, n_jobs=-1, verbose=verbose)
+
+grid.fit(question_loader.questions, question_loader.categoryids)
+shared.save_and_report(results=grid.cv_results_, folder='ensemble')
