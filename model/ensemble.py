2344fd1c56f446ac9cfc0f7c9c1e32e6e3177efc
Ingo Guehring <i.guehring@gmx.de>
2018-02-18 13:53:35 +0100

    refacto voting classifier B

--- model/ensemble.py
+++ model/ensemble.py
@@ -13,26 +13,10 @@ class VotingClassifierB(VotingClassifier):
     """
     """
     def __init__(self, estimators, voting='hard', weights=None, n_jobs=1,
-                 flatten_transform=None, save_probas=False, save_avg=False):
+                 flatten_transform=None, save_avg=False):
         super().__init__(estimators, voting='hard', weights=None, n_jobs=1,
                          flatten_transform=None)
-        self.save_probas = save_probas
         self.save_avg = save_avg
-
-    def _collect_probas(self, X):
-        """Collect results from clf.predict calls. """
-        probas = [clf.predict_proba(X) for clf in self.estimators_]
-        if self.save_probas:
-            names, _ = zip(*self.estimators)
-            probas_names = list(zip(probas, names))
-
-            # save results
-            current_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
-            path = './results/gridsearch/ensemble/raw/'
-            filename = current_time + '-'.join(names)
-            np.savez(path + filename, probas=probas_names,
-                     classes=self.le_.classes_)
-            return np.asarray(probas)
 
     def _predict_proba(self, X):
         """Predict class probabilities for X in 'soft' voting """
