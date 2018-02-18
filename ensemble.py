3814e6f669a216cd5ee8b37443277f41b1d18547
Ingo Guehring <i.guehring@gmx.de>
2018-02-18 20:24:09 +0100

    voting classifier saves raw data

--- ensemble.py
+++ ensemble.py
@@ -1,19 +1,18 @@
-"""
-The :mod: `ensemble` combines multiple classifiers in a voting schema
-"""
+""" The :mod: `ensemble` combines multiple classifiers in a voting schema """
 # Author: Ingo Guehring
 
 from sklearn.ensemble import VotingClassifier
-from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import cross_val_score
 import numpy as np
 
 import model.question_loader as ql
+from model.ensemble import GridSearchCVB
 import evaluation.shared as shared
 
-
+# If a classifier is changed the grid might have to be changed, too
 ensemble = VotingClassifier(
     estimators=[('mnb', shared.MNB),
-                ('logreg', shared.LOGREG),
+                ('svm', shared.SVM),
                 ('lda', shared.LDA)], voting='soft')
 
 subcats = False
@@ -36,19 +35,19 @@ question_loader = ql.QuestionLoader(
 C_RANGE = np.logspace(-5, 5, 11)
 
 # grid
-PARAM_GRID = {'logreg__classifier__C': C_RANGE}
+PARAM_GRID = {'svm__classifier__C': C_RANGE}
 
-grid = GridSearchCV(
-    estimator=ensemble, cv=cv, param_grid=PARAM_GRID, scoring='f1_macro',
-    refit=False, error_score=-1, n_jobs=-1, verbose=verbose)
+# grid = GridSearchCV(
+#     estimator=ensemble, cv=cv, param_grid=PARAM_GRID, scoring='f1_macro',
+#     refit=False, error_score=-1, n_jobs=-1, verbose=verbose)
+#
+# grid.fit(question_loader.questions, question_loader.categoryids)
+# print(grid.cv_results_)
+# shared.save_and_report(results=grid.cv_results_, folder='ensemble')
 
-grid.fit(question_loader.questions, question_loader.categoryids)
-print(grid.cv_results_)
-shared.save_and_report(results=grid.cv_results_, folder='ensemble')
 
-
-clf = GridSearchCV(estimator=ensemble, param_grid=PARAM_GRID, cv=cv,
-                   n_jobs=-1, scoring='f1_macro', verbose=verbose)
+clf = GridSearchCVB(estimator=ensemble, param_grid=PARAM_GRID, cv=cv,
+                    n_jobs=-1, scoring='f1_macro', verbose=verbose)
 nested_cv_scores = cross_val_score(
     clf, X=question_loader.questions, y=question_loader.categoryids, cv=cv,
     scoring='f1_macro', verbose=verbose)
